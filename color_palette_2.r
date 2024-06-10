fa523224885e4692157c5a441ca3ea07202479dc
Joseph Hess <162132971+hess80@users.noreply.github.com>
2024-06-10 09:20:28 -0400

    Add files via upload

---
+++ color_palette_2.r
@@ -0,0 +1,8 @@
+########## List of colors
+pal = function(){c('#0057a5', '#002859', '#3ac1cc', '#f26f64', '#00bcf2', '#009f9d',
+            '#963d24', '#0070a6', '#b88d80', '#faa85d', 
+            '#d8d9da', '#bcbec0', '#939598', '#58595b', '#ebebec', '#000000', '#FFFFFF')}
+gpal = function(){c('#d8d9da', '#bcbec0', '#939598', '#58595b', '#ebebec', '#000000', '#FFFFFF')}
+cpal = function(){c('#0057a5', '#002859', '#3ac1cc', '#f26f64', '#00bcf2', '#009f9d',
+            '#963d24', '#0070a6', '#b88d80', '#faa85d', '#000000', '#FFFFFF')}
+Opal = function(){palette()}
